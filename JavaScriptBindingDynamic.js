21cbc46c9513ed3892facd53fab325c5e0d6ac5e
LavenderPixl <annajacobsen1999@hotmail.com>
2023-09-01 09:02:13 +0200

    Setting up Repository.
    JavaScript is done, and working.

---
+++ JavaScriptBindingDynamic.js
@@ -0,0 +1,78 @@
+﻿var ButtonCounter = 0;
+var TextBoxElement;
+var DocumentButton;
+
+
+function SetupDOMElements() {
+    TextBoxElement = document.getElementById("txtButtonContext");
+}
+function AddBaseButton()
+{
+    var button = document.createElement('input');
+    button.setAttribute('type', 'button');
+    button.setAttribute('ID', "btn" + ButtonCounter.toString());
+    button.setAttribute('value', "btnShow" + ButtonCounter.toString());
+    button.setAttribute('onclick', "AddedButtonClicked(" + ButtonCounter + ")");
+    button.style.backgroundColor = "Blue";
+    button.style.color = "White";
+    button.style.height = "40px";
+    return button;
+}
+function AddExtraButtons(text, clickFunc)
+{
+    var btn = document.createElement('input');
+    btn.setAttribute('value', text);
+    btn.setAttribute('onclick', clickFunc);
+    btn.setAttribute('type', 'button');
+    return btn;
+}
+
+function AddNewButton() {
+    var div = document.createElement('div');
+    div.setAttribute('ID', "btn"+ButtonCounter.toString());
+    document.getElementsByClassName("ButtonsAdded")[0].appendChild(div);
+    
+    var button = AddBaseButton();
+    var delButton = AddExtraButtons("Delete", "DeleteButton("+ div.getAttribute("ID")+")");
+    var editButton = AddExtraButtons("Edit", "EditButton("+div.getAttribute("ID")+")");
+    // TextBoxElement.style.visibility = "hidden";
+    
+    div.appendChild(button);
+    div.appendChild(delButton);
+    div.appendChild(editButton);
+
+    ButtonCounter++;
+}
+
+function DeleteButton(div) 
+{
+    div.remove()
+    // document.getElementsByClassName("ButtonsAdded")[e.remove()]
+    // e.parentElement.remove()
+}
+function EditButton(div)
+{
+    TextBoxElement.style.visibility = "visible";
+    var btn = div.querySelector("#"+div.id+"");
+    AddedButtonClicked(btn);
+
+}
+
+function AddedButtonClicked(btn) {
+    DocumentButton = btn;
+    TextBoxElement.value = DocumentButton.value;
+    TextBoxElement.style.width = CalculateWidthOnControl(TextBoxElement);
+    console.log(TextBoxElement.style.width);
+}
+
+function TextboxValueChanged() {
+    DocumentButton.value = TextBoxElement.value;
+    TextBoxElement.style.width = CalculateWidthOnControl(TextBoxElement);
+    console.log(TextBoxElement.style.width);
+}
+
+function CalculateWidthOnControl(Control_Object) {
+    ControlWidth = Control_Object.value.length;
+    console.log(ControlWidth);
+    return (ControlWidth * 9 + 25 + 'px');
+}
---
+++ jQueryBindingDynamic.js
@@ -0,0 +1,48 @@
+﻿var ButtonAddedCounter = 0;
+var CurrentButton;
+
+var txtButtonContext = $("#txtButtonContext");
+var ButtonAddedClass = $(".ButtonsAdded");
+
+$("#btnAddNewButton").on('click', function () {
+    var div = $("<div id='div"+ ButtonAddedCounter +"'></div>");
+    var Button = $("<input type='button' id='btnDynamic" + (++ButtonAddedCounter).toString() + "' value = 'btnDynamic" + ButtonAddedCounter + "' /> ");
+    var edit = $("<input type='button' id='btnEdit' value = 'Edit' /> ");
+    var del = $("<input type='button' class='btnDelete' value = 'Delete' /> ");
+    div.append(Button);
+    div.append(edit);
+    div.append(del);
+    ButtonAddedClass.append(div);
+    ButtonAddedClass.append("<br />");
+});
+
+
+// ButtonAddedClass.delegate("input", "click", function () {
+//     CurrentButton = $(this);
+//     var TextToTextField = $(this).val();
+//     txtButtonContext.val(TextToTextField);
+//     txtButtonContext.width(CalculateWidthOnControl(txtButtonContext));
+// });
+
+
+txtButtonContext.on('input', function () {
+    CurrentButton.val($(this).val());
+    txtButtonContext.width(CalculateWidthOnControl(txtButtonContext));
+});
+
+function CalculateWidthOnControl(Control_Object) {
+    ControlWidth = Control_Object.val().length;
+    return (ControlWidth * 9 + 25);
+}
+
+function deleteButton() {
+    console.log("eeee");
+}
+
+$('.btnDelete').live('click', function ()
+{
+    $('input').remove('.btnDelete');
+
+    // $(this).parent().remove();
+    // console.log("fff");
+});
